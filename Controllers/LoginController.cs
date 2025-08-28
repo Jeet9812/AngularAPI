39b66a0a5f9269d3e3acaaadbf8f066a5c2be219
Jeet9812 <Jeet.chauhan@eptl.in>
2025-08-28 16:03:36 +0530

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using AngularAPI.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AngularAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequest login)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            string storedProcedure = "dbo.CheckLogin";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                var parametersq = new DynamicParameters();
+                parametersq.Add("@UserName", login.UserName);
+                parametersq.Add("@UserPassword", login.UserPassword);
+                try
+                {
+                    var result = await connection.QueryFirstOrDefaultAsync<string>(
+                    storedProcedure,
+                    parametersq,
+                    commandType: CommandType.StoredProcedure);
+
+                    if (result == "Login Successful")
+                    {
+                        return Ok(new { Message = result });
+                    }
+                    else
+                    {
+                        return Unauthorized(new { Message = result });
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while processing the request.", Error = ex.Message });
+                }
+            }
+        }
+    }
+}
---
+++ Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AngularAPI.Models
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
